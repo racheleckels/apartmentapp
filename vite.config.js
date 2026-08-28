f864b7bc79fe1b0d2951ee2f94daeb4579fd8d19
Rachel Eckels <racheleckels@Rachels-MacBook-Air.local>
2026-08-28 01:24:18 -0500

    revert vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react' 
-import { defineConfig } from 'vite' 
-export default defineConfig({ 
-  plugins: [react()], 
-  base: 'apartmentapp', 
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
